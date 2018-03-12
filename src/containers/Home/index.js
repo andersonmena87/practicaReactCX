b880ca65d4583a80f9158a7b66d821f562e496a9
anderson <amena@cognox.com>
2018-03-12 15:30:07 -0500

    third commit

---
+++ src/containers/Home/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class Home extends Component {
+  static proptypes = {
+    isMobile: PropTypes.bool
+  };
+
+  render() {
+    const { isMobile } = this.props;
+    return (
+      <div className="Home">
+        <h1>Home Page</h1>
+        <p>
+          {isMobile ? 'Mobile device' : 'Desktop device'}
+        </p>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    isMobile: state.device.isMobile
+  };
+}
+
+export default connect(state => ({
+  isMobile: state.device.isMobile
+}), null)(Home);
--- src/index.js
+++ src/index.js
@@ -1,19 +1,44 @@
-// Dependencias
+// Dependencies
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Bluebird from 'bluebird';
+import { Provider } from 'react-redux';
 
 // Routes
 import Routes from './routes';
 
 //Assets
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+
+// Reducers store
+import configureStore from './lib/configureStore';
+
+// Reducers
+import rootReducer from './reducers';
+
+// Bluebird configuration
+window.Promise = Bluebird;
+
+Bluebird.config({ warnings: false });
+
+window.addEventListener('unhandlerejection', error => {
+  error.preventDefault();
+
+  if (process.env.NODE_ENV !== 'production') {
+    console.warn('Unhandled promise rejection warning.', error.detail);
+  }
+});
+
+const store = configureStore({
+  initialState: window.initialState
+}, rootReducer);
 
 render(
-  <Router>
-    <Routes />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <Routes />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
-registerServiceWorker();
