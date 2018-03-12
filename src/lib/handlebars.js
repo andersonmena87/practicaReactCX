cbbe503181d72fcce0a477e659c7cfd3f7470c81
anderson <amena@cognox.com>
2018-03-12 11:28:49 -0500

    second commit

--- src/lib/handlebars.js
+++ src/lib/handlebars.js
@@ -3,7 +3,7 @@ import  { minify } from 'html-minifier';
 
 export function compress(content) {
   return minify(content.fn(this), {
-    removeComment: true,
+    removeComments: true,
     collapseWhitespace: true,
     minifyJS: true
   })
--- src/server/index.js
+++ src/server/index.js
@@ -16,6 +16,9 @@ import blogApi from './api/blog';
 // Helpers
 import * as hbsHelper from '../lib/handlebars';
 
+// Utlis
+import { isMobile } from '../lib/utils/device';
+
 //Server Port
 const port = 3000;
 
@@ -28,6 +31,16 @@ const app = express();
 // Public
 app.use(express.static(path.join(__dirname, '../public')));
 
+// Handlebars setup
+app.engine('.hbs', exphbs({
+  extname: '.hbs',
+  helpers: hbsHelper,
+}));
+
+// View Engine Setup
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', '.hbs');
+
 //Webpack Compiler
 const webpackCompiler = webpack(webpackConfig);
 
@@ -36,15 +49,24 @@ if (isDevelmont) {
   app.use(webpackHotMiddleware(webpackCompiler));
 }
 
+// Device detector
+app.use((req, res, next) => {
+  res.locals.isMobile = isMobile(req.headers['user-agent']);
+
+  return next();
+});
+
 // API dispatch
 app.use('/api/blog', blogApi);
 
 // Sending all traffic to React
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-})
+  res.render('frontend/index', {
+    layout: false
+  });
+});
 
-// Listen port
+// Listen port 3000
 app.listen(port, err => {
   if (!err) {
     open(`http://localhost:${port}`);
